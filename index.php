b0dfb5334f52e45372e341f03446b73e88512cd3
El Stevens <eleanorkwstevens@gmail.com>
2020-09-28 16:32:44 +0100

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Cartoon Collection</title>
+</head>
+
+<body>
+
+<header>
+    <h1>My Cartoon Collection</h1>
+</header>
+
+
+
+
+
 <?php
+
+$db = new PDO('mysql:host=db;dbname=MyCollection', 'root', 'password'); // initialise the db connection
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$query = $db->prepare('SELECT  `cartoons`.`character_name`, `TVshows`.`name`, `IQ`, `images` FROM `cartoons`
+JOIN `TVshows` ON `cartoons`.`TVshow_id` = `TVshows`.`id`;');
+$query->execute();
+$cartoons=$query->fetchAll();
+
+foreach ($cartoons as $character) {
+  $characterHtmlElement = '';
+  $characterNameElement = '<h2>' . $character['character_name'] . '</h2>';
+  $characterTvShowElement = '<h3>' . $character['name'] . '</h3>';
+  $characterIqElement = '<h4>' . $character['IQ'] . '</h4>';
+  $characterHtmlElement = '<div class="container">' . $characterNameElement . $characterTvShowElement . $characterIqElement . '</div>';
+  echo $characterHtmlElement;
+};
+
+?>
+
+
+</body>
+</html>
