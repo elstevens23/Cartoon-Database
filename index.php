ebe70df377076c13c441ea52997d8e62cc19c81a
El Stevens <eleanorkwstevens@gmail.com>
2020-09-29 13:24:58 +0100

    cartoons-stylesheet.css

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Cartoon Collection</title>
+    <link href="cartoons-stylesheet.css" type="text/css" rel="stylesheet">
+    <link href="normalize.css" type="text/css" rel="stylesheet">
+    <link href="cartoon-font.ttf" type="text/ttf" rel="stylesheet">
+    <link href="BalooTammudu2-Regular.ttf" type="text/ttf" rel="stylesheet">
 </head>
 
 <body>
 
-<header>
+<div class="mainTitle">
     <h1>My Cartoon Collection</h1>
-</header>
-
-
+</div>
 
 
 
@@ -19,19 +22,24 @@
 
 $db = new PDO('mysql:host=db;dbname=MyCollection', 'root', 'password'); // initialise the db connection
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare('SELECT  `cartoons`.`character_name`, `TVshows`.`name`, `IQ`, `images` FROM `cartoons`
+$query = $db->prepare('SELECT  `cartoons`.`character_name`, `TVshows`.`name`, `IQ`, `image` FROM `cartoons`
 JOIN `TVshows` ON `cartoons`.`TVshow_id` = `TVshows`.`id`;');
 $query->execute();
 $cartoons=$query->fetchAll();
 
+echo '<div class="characters">';
+
 foreach ($cartoons as $character) {
-  $characterHtmlElement = '';
-  $characterNameElement = '<h2>' . $character['character_name'] . '</h2>';
-  $characterTvShowElement = '<h3>' . $character['name'] . '</h3>';
-  $characterIqElement = '<h4>' . $character['IQ'] . '</h4>';
-  $characterHtmlElement = '<div class="container">' . $characterNameElement . $characterTvShowElement . $characterIqElement . '</div>';
+    $characterHtmlElement = '';
+    $characterImageElement = '<img src="' .$character['image'] . '" alt="Picture of' . $character['character_name'] . '">';
+    $characterNameElement = $character['character_name'];
+    $characterTvShowElement = $character['name'];
+    $characterIqElement = $character['IQ'];
+    $characterHtmlElement = '<div class="container">' . $characterImageElement . "<h2>Name: " . $characterNameElement . '</h2>' . "<h2>TV Show: " . $characterTvShowElement . '</h2>' . "<h2>IQ: " . $characterIqElement . '</h2>' . '</div>';
   echo $characterHtmlElement;
 };
+
+echo '</div>';
 
 ?>
 
