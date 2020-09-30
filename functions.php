0fcbf14ec958098d34da223be6131eb3556a47a4
El Stevens <eleanorkwstevens@gmail.com>
2020-09-30 13:33:36 +0100

    updating files

--- functions.php
+++ functions.php
@@ -1,15 +1,20 @@
 <?php
 
-function eachCharacter($cartoons)
+function eachCharacter(array $cartoons): string
 {
+    $characterHtmlElementsString = '';
     foreach ($cartoons as $character) {
-        $characterImageElement = '<img src="' . $character['image'] . '" alt="Picture of' . $character['character_name'] . '">';
+        $characterImageElement = '<img src="' . $character['image'] . '" alt="Picture of ' . $character['character_name'] . '">';
         $characterNameElement = $character['character_name'];
         $characterTvShowElement = $character['name'];
         $characterIqElement = $character['IQ'];
         $characterHtmlElement = '<div class="container">' . $characterImageElement . "<h2>Name: " . $characterNameElement . '</h2>' . "<h2>TV Show: " . $characterTvShowElement . '</h2>' . "<h2>IQ: " . $characterIqElement . '</h2>' . '</div>';
-        echo $characterHtmlElement;
+        $characterHtmlElementsString .= $characterHtmlElement;
+        if (!isset($character['character_name']) || !isset($character['IQ']) || !isset($character['name']) || !isset($character['image'])) {
+            return false;
+        };
     };
+    return $characterHtmlElementsString;
 }
 
 function connect_db($collectionName)
