3ffb6e891812d5fee5d44243d16f123886e87ae1
El Stevens <eleanorkwstevens@gmail.com>
2020-10-01 10:18:20 +0100

    changes for code review

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<?php
+
+require 'functions.php';
+
+$db = connect_db('CartoonCollection');
+$cartoons = extract_from_db($db);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Cartoon Collection</title>
-    <link href="input-newdata-form.php" type="text/php" rel="stylesheet">
     <link href="cartoons-stylesheet.css" type="text/css" rel="stylesheet">
     <link href="normalize.css" type="text/css" rel="stylesheet">
     <link href="cartoon-font.ttf" type="text/ttf" rel="stylesheet">
@@ -20,10 +28,6 @@
 
 
 <?php
-
-include ('functions.php');
-
-$cartoons = connect_db_return_collection('CartoonCollection');
 
 echo '<div class="characters">';
      foreach ($cartoons as $character) {
